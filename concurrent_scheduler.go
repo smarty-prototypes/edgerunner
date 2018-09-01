41f7f519e85cb32d67db281c72118e010d69611b
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-08-31 19:51:53 -0600

    Closer to what we want.

--- concurrent_scheduler.go
+++ concurrent_scheduler.go
@@ -34,41 +34,32 @@ func (this *ConcurrentScheduler) Schedule() error {
 	}
 
 	closeChannel(this.current) // signal the current instance to terminate
-	this.waiter.Wait()         // wait for all instantiated tasks to 100% completely finish
+	this.waiter.Wait()         // wait for all outstanding tasks to 100% completely finish
 	return this.loadError()    // did the most recent task have any problem initializing?
 }
 func (this *ConcurrentScheduler) schedule() bool {
 	this.waiter.Add(1) // another task has started
 
-	//	this.previous = this.current
 	if this.loadError() == nil {
+		this.previous = this.current
 		this.current = make(chan struct{}, 2) // the previous task started successfully
 	}
+
 	task := this.factory()
 
-	go this.runTask(task, this.previous, this.current)
+	go this.runTask(task, this.previous)
 	go this.watchSignal(task, this.current)
-
 	return this.reader.Read() // blocking
 }
-func (this *ConcurrentScheduler) runTask(task Task, previous, current chan struct{}) {
+func (this *ConcurrentScheduler) runTask(task Task, previous chan struct{}) {
 	defer this.waiter.Done() // task has finished
 
 	if this.storeError(task.Init()) {
 		return // initialization failed; don't close the previous task
 	}
 
-	// TODO: safe locking
-	this.previous = current
-
 	closeChannel(previous) // signal the previous task (if any) that we're starting to listen
 	task.Listen()
-	// TODO: what happens when task.Listen() exits without task.Close()?
-	// if so, we need a way to signal main loop about what's happening...
-	// this probably means we pull the main loop out one more layer
-	// and have a signal for it that we can make here
-	// because if a given task.Listen() fails without close being called, we need
-	// to shut down
 }
 func (this *ConcurrentScheduler) watchSignal(task io.Closer, signal chan struct{}) {
 	<-signal
