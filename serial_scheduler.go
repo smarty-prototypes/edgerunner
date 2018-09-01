a282302eb94f937dc5de11a1fe934384ffb323aa
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-09-01 07:58:58 -0600

    Don't mutate state in a function that reads.

--- serial_scheduler.go
+++ serial_scheduler.go
@@ -21,31 +21,32 @@ func (this *SerialScheduler) Schedule() error {
 	}
 	return this.err
 }
-
 func (this *SerialScheduler) scheduleTask() bool {
+	this.resetScheduleAgain()
+
 	task := this.factory()
 	go this.watchSignal(task)
 
 	if this.err = task.Init(); this.err != nil {
-		return false
+		return false // task.Close() will be called when a signal arrives or the reader closes
 	} else {
 		task.Listen() // we only schedule again if listen exits correctly
 	}
 
 	return this.canScheduleAgain()
 }
-
 func (this *SerialScheduler) watchSignal(task Task) {
 	reload := this.reader.Read()
 	err := task.Close()
 	this.scheduleAgain(reload && err == nil)
 }
 
+func (this *SerialScheduler) resetScheduleAgain() {
+	atomic.StoreUint32(&this.again, 0)
+}
 func (this *SerialScheduler) canScheduleAgain() bool {
-	defer atomic.StoreUint32(&this.again, 0) // reset state
 	return atomic.LoadUint32(&this.again) == 1
 }
-
 func (this *SerialScheduler) scheduleAgain(again bool) {
 	if again {
 		atomic.StoreUint32(&this.again, 1)
