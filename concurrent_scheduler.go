43fff400736bf1ea55bf553d4d0f266621891511
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-08-31 18:38:11 -0600

    Experimenting with concurrent scheduler.

--- concurrent_scheduler.go
+++ concurrent_scheduler.go
@@ -5,6 +5,10 @@ import (
 	"sync"
 )
 
+// Issues:
+// 1. if Listen() exits without Close() being called
+// 2. if Init() returns an error
+
 type ConcurrentScheduler struct {
 	reader  SignalReader
 	factory TaskFactory
@@ -34,30 +38,37 @@ func (this *ConcurrentScheduler) Schedule() error {
 	return this.loadError()    // did the most recent task have any problem initializing?
 }
 func (this *ConcurrentScheduler) schedule() bool {
-	task := this.prepare()
-	go this.runTask(task, this.previous)
+	this.waiter.Add(1) // another task has started
+
+	//	this.previous = this.current
+	if this.loadError() == nil {
+		this.current = make(chan struct{}, 2) // the previous task started successfully
+	}
+	task := this.factory()
+
+	go this.runTask(task, this.previous, this.current)
 	go this.watchSignal(task, this.current)
+
 	return this.reader.Read() // blocking
 }
-func (this *ConcurrentScheduler) prepare() Task {
-	this.waiter.Add(1) // another task has started
-	this.previous = this.current
-	if this.loadError() == nil {
-		// this previous task started properly, so now we can create a new channel
-		this.current = make(chan struct{}, 2)
-	}
-
-	return this.factory()
-}
-func (this *ConcurrentScheduler) runTask(task Task, signal chan struct{}) {
+func (this *ConcurrentScheduler) runTask(task Task, previous, current chan struct{}) {
 	defer this.waiter.Done() // task has finished
 
 	if this.storeError(task.Init()) {
-		return // initialize failed
+		return // initialization failed; don't close the previous task
 	}
 
-	closeChannel(signal) // signal the previous task (if any) that we're starting to listen
+	// TODO: safe locking
+	this.previous = current
+
+	closeChannel(previous) // signal the previous task (if any) that we're starting to listen
 	task.Listen()
+	// TODO: what happens when task.Listen() exits without task.Close()?
+	// if so, we need a way to signal main loop about what's happening...
+	// this probably means we pull the main loop out one more layer
+	// and have a signal for it that we can make here
+	// because if a given task.Listen() fails without close being called, we need
+	// to shut down
 }
 func (this *ConcurrentScheduler) watchSignal(task io.Closer, signal chan struct{}) {
 	<-signal
--- sample/main.go
+++ sample/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
 	"log"
-	"net"
-	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"sync/atomic"
 	"syscall"
+	"time"
 
 	"github.com/smartystreets/edgerunner"
 )
@@ -37,187 +37,47 @@ func main() {
 }
 
 func newScheduler(reader edgerunner.SignalReader) edgerunner.Scheduler {
-	return edgerunner.NewConcurrentScheduler(reader, NewWebTask)
-	//return edgerunner.NewSerialScheduler(reader, NewWebTask)
+	return edgerunner.NewConcurrentScheduler(reader, NewSimpleTask)
 }
 
 /////////////////////////////////////////////////////////////////
 
-type WebTask struct {
-	inner    edgerunner.Task
-	output   chan int8
-	server   *http.Server
-	listener net.Listener
-	counter  int32
+var counter uint64
+
+type SimpleTask struct {
+	id     uint64
+	waiter *sync.WaitGroup
 }
 
-func NewWebTask() edgerunner.Task {
-	this := &WebTask{
-		output: make(chan int8, 16),
+func NewSimpleTask() edgerunner.Task {
+	id := atomic.AddUint64(&counter, 1)
+	return &SimpleTask{id: id, waiter: &sync.WaitGroup{}}
+}
+
+func (this *SimpleTask) Init() error {
+	log.Printf("%d initializing...", this.id)
+	this.waiter.Add(1)
+	time.Sleep(time.Second)
+
+	if this.id%2 == 0 {
+		log.Printf("%d initialized FAILED", this.id)
+		return errors.New("init failed")
 	}
 
-	this.inner = NewSanitizeTask(this.output)
-	this.server = &http.Server{Handler: this}
-
-	return this
-}
-func (this *WebTask) Init() error {
-	this.inner.Init()
-	log.Println("Web Init")
-
-	config := &net.ListenConfig{Control: reusePort}
-
-	// this is where we take control of the port
-	if listener, err := config.Listen(context.Background(), "tcp", "127.0.0.1:8080"); err != nil {
-		return err
-	} else {
-		this.listener = listener
-	}
-
+	log.Printf("%d initialized", this.id)
 	return nil
 }
 
-func reusePort(network, address string, conn syscall.RawConn) error {
-	return conn.Control(func(descriptor uintptr) {
-		syscall.SetsockoptInt(int(descriptor), syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1)
-	})
+func (this *SimpleTask) Listen() {
+	log.Printf("%d listening...", this.id)
+	this.waiter.Wait()
+	log.Printf("%d listen completed", this.id)
 }
 
-func (this *WebTask) Listen() {
-	go this.listen()
-	this.inner.Listen()
-}
-func (this *WebTask) listen() {
-	log.Println("Web Listening...")
-
-	this.server.Serve(this.listener) // this should block
-	this.server.Shutdown(context.Background())
-	close(this.output)
-	this.listener.Close()
-
-	log.Println("Web Listen completed")
-}
-
-func (this *WebTask) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	value := int8(atomic.AddInt32(&this.counter, 1))
-	log.Println("Web Listen", value)
-	this.output <- value
-}
-
-func (this *WebTask) Close() error {
-	log.Println("Web Closing")
-	this.server.Close()
-	log.Println("Web marked server as closed")
+func (this *SimpleTask) Close() error {
+	log.Printf("%d closing...", this.id)
+	time.Sleep(time.Millisecond * 500)
+	this.waiter.Done()
+	log.Printf("%d marked as closed", this.id)
 	return nil
 }
-
-///////////////////////////////////////////////////////////////////
-
-type SanitizeTask struct {
-	inner  edgerunner.Task
-	input  chan int8
-	output chan int16
-}
-
-func NewSanitizeTask(input chan int8) edgerunner.Task {
-	output := make(chan int16, 16)
-	return &SanitizeTask{
-		inner:  NewVerifyTask(output),
-		input:  input,
-		output: output,
-	}
-}
-func (this *SanitizeTask) Init() error {
-	this.inner.Init()
-	log.Println("Sanitize Init")
-	return nil
-}
-func (this *SanitizeTask) Listen() {
-	go this.listen()    // start my listener in the background
-	this.inner.Listen() // wait for inner listener to complete before we exit
-
-}
-func (this *SanitizeTask) listen() {
-	log.Println("Sanitize Listening...")
-	for item := range this.input {
-		log.Println("Sanitize:", item)
-		this.output <- int16(item)
-	}
-
-	close(this.output)
-	log.Println("Sanitize Listen completed")
-}
-
-func (this *SanitizeTask) Close() error {
-	return nil
-}
-
-///////////////////////////////////////////////////////////////////
-
-type VerifyTask struct {
-	inner  edgerunner.Task
-	input  chan int16
-	output chan int32
-}
-
-func NewVerifyTask(input chan int16) edgerunner.Task {
-	output := make(chan int32, 16)
-	return &VerifyTask{
-		inner:  NewLogTask(output),
-		input:  input,
-		output: output,
-	}
-}
-func (this *VerifyTask) Init() error {
-	this.inner.Init()
-	log.Println("Verify Init")
-	return nil
-}
-func (this *VerifyTask) Listen() {
-	go this.listen()    // start my listener in the background
-	this.inner.Listen() // wait for inner listener to complete before we exit
-
-}
-func (this *VerifyTask) listen() {
-	log.Println("Verify Listening...")
-
-	for item := range this.input {
-		log.Println("Verify:", item)
-		this.output <- int32(item)
-	}
-
-	close(this.output)
-	log.Println("Verify Listen completed")
-}
-
-func (this *VerifyTask) Close() error {
-	return nil
-}
-
-///////////////////////////////////////////////////////////////////
-
-type LogTask struct {
-	input chan int32
-}
-
-func NewLogTask(input chan int32) edgerunner.Task {
-	return &LogTask{input: input}
-}
-func (this *LogTask) Init() error {
-	log.Println("Log Init")
-	return nil
-}
-func (this *LogTask) Listen() {
-	log.Println("Log Listening...")
-
-	for item := range this.input {
-		log.Println("Log:", item)
-	}
-
-	log.Println("Log Listen completed")
-}
-func (this *LogTask) Close() error {
-	return nil
-}
-
-///////////////////////////////////////////////////////////////////
