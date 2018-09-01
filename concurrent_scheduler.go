9227d421a519535f5e3f74cec574cb2faf7ab696
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-09-01 09:38:47 -0600

    Vastly simplier scheduler--only missing premature Listen() exit handling.

--- concurrent_scheduler.go
+++ concurrent_scheduler.go
@@ -1,18 +1,13 @@
 package edgerunner
 
-import (
-	"io"
-	"sync"
-	"sync/atomic"
-)
+import "sync"
 
 type ConcurrentScheduler struct {
 	reader        SignalReader
 	factory       TaskFactory
 	waiter        *sync.WaitGroup
 	startupResult chan error
-	head          *SafeTask
-	counter       uint32
+	head          Task
 	err           error
 }
 
@@ -35,66 +30,34 @@ func (this *ConcurrentScheduler) Schedule() error {
 	return this.err
 }
 func (this *ConcurrentScheduler) schedule() bool {
-	if !this.head.IsSafe() {
-		return false
-	}
-
 	this.waiter.Add(1)
-	task := &SafeTask{Task: this.factory()}
-	previous := this.head
-	go this.runTask(task, previous)
+
+	proposed, previous := this.factory(), this.head
+	go this.runTask(proposed, previous)
 
 	if this.err = <-this.startupResult; this.err != nil {
-		task.Close()
+		proposed.Close()
 	} else {
-		this.head = task
+		this.head = proposed
 	}
 
-	if this.counter == 0 && this.err != nil {
-		return false
-	}
-
-	this.counter++
-	return this.reader.Read() && this.head.IsSafe()
+	return this.reader.Read()
 }
 
-func (this *ConcurrentScheduler) runTask(proposed Task, previous io.Closer) {
+func (this *ConcurrentScheduler) runTask(proposed, previous Task) {
 	defer this.waiter.Done()
 	if !this.initializeTask(proposed) {
 		return
 	}
 
-	// note: semi-concurrent = previous.Close(), fully concurrent = go previous.Close()
-	previous.Close()
-	proposed.Listen()
+	if previous != nil {
+		previous.Close() // note: semi-concurrent = previous.Close(), fully concurrent = go previous.Close()
+	}
 
+	proposed.Listen()
 }
 func (this *ConcurrentScheduler) initializeTask(task Task) bool {
 	err := task.Init()
 	this.startupResult <- err
 	return err == nil
-}
-
-/////////////////////////////////////////////////////////////////
-
-type SafeTask struct {
-	Task
-	state uint32
-}
-
-func (this *SafeTask) Close() error {
-	if this == nil {
-		return nil
-	} else if atomic.CompareAndSwapUint32(&this.state, 0, 1) {
-		return this.Task.Close()
-	} else {
-		panic("task already closed")
-	}
-}
-func (this *SafeTask) Listen() {
-	this.Task.Listen()
-	atomic.CompareAndSwapUint32(&this.state, 0, 2)
-}
-func (this *SafeTask) IsSafe() bool {
-	return this == nil || atomic.LoadUint32(&this.state) != 2
 }
