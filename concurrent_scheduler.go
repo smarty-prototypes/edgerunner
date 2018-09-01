ccd51315539956b39f7c98825de8180c5e9a8ab6
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-09-01 13:10:05 -0600

    Fully concurrent scheduler completed with the following properties:
    1. If the first task fails to initialize, the runner returns control to the caller
    2. The currently running (head) task doesn't surrender control until the upcoming task has completely initialized.
    3. If the head task exits without Close() being called on it explictly the runner returns control to the caller.
    4. Initialization errors are returned to the caller.

--- concurrent_scheduler.go
+++ concurrent_scheduler.go
@@ -1,41 +1,49 @@
 package edgerunner
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 type ConcurrentScheduler struct {
-	reader        SignalReader
-	factory       TaskFactory
-	waiter        *sync.WaitGroup
-	startupResult chan error
-	head          Task
-	err           error
+	reader   SignalReader
+	factory  TaskFactory
+	waiter   *sync.WaitGroup
+	startup  chan error
+	shutdown chan struct{}
+	head     *ConcurrentTask
+	err      error
 }
 
 func NewConcurrentScheduler(reader SignalReader, factory TaskFactory) *ConcurrentScheduler {
 	return &ConcurrentScheduler{
-		reader:        reader,
-		factory:       factory,
-		waiter:        &sync.WaitGroup{},
-		startupResult: make(chan error, 2),
+		reader:   reader,
+		factory:  factory,
+		waiter:   &sync.WaitGroup{},
+		startup:  make(chan error, 2),
+		shutdown: make(chan struct{}, 2),
 	}
 }
 
 func (this *ConcurrentScheduler) Schedule() error {
-	for this.schedule() {
-	}
+	go func() {
+		for this.schedule() {
+		}
 
-	this.head.Close()
-	this.waiter.Wait()
+		this.shutdown <- struct{}{} // signal the shutdown channel that we're ready to close
+	}()
 
+	<-this.shutdown
+	this.cleanup()
 	return this.err
 }
 func (this *ConcurrentScheduler) schedule() bool {
 	this.waiter.Add(1)
 
-	proposed, previous := this.factory(), this.head
-	go this.runTask(proposed, previous)
+	proposed := &ConcurrentTask{Task: this.factory(), signal: this.shutdown}
+	go this.runTask(proposed, this.head)
 
-	if this.err = <-this.startupResult; this.err != nil {
+	if this.err = <-this.startup; this.err != nil {
 		proposed.Close()
 	} else {
 		this.head = proposed
@@ -46,18 +54,42 @@ func (this *ConcurrentScheduler) schedule() bool {
 
 func (this *ConcurrentScheduler) runTask(proposed, previous Task) {
 	defer this.waiter.Done()
-	if !this.initializeTask(proposed) {
-		return
-	}
-
-	if previous != nil {
+	if this.initializeTask(proposed) {
 		previous.Close() // note: semi-concurrent = previous.Close(), fully concurrent = go previous.Close()
+		proposed.Listen()
 	}
-
-	proposed.Listen()
 }
 func (this *ConcurrentScheduler) initializeTask(task Task) bool {
 	err := task.Init()
-	this.startupResult <- err
+	this.startup <- err
 	return err == nil
+}
+
+func (this *ConcurrentScheduler) cleanup() {
+	this.head.Close()
+	this.waiter.Wait()
+	close(this.shutdown)
+	close(this.startup)
+}
+
+///////////////////////////////////////////////////////////////////////
+
+type ConcurrentTask struct {
+	Task
+	signal chan struct{}
+	state  uint32
+}
+
+func (this *ConcurrentTask) Listen() {
+	this.Task.Listen()
+	if !atomic.CompareAndSwapUint32(&this.state, 1, 2) {
+		this.signal <- struct{}{} // didn't shutdown cleanly
+	}
+}
+func (this *ConcurrentTask) Close() error {
+	if this != nil && atomic.CompareAndSwapUint32(&this.state, 0, 1) {
+		return this.Task.Close()
+	}
+
+	return nil
 }
